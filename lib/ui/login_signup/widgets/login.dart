e7c92f636a41c6d755779db769e52ff1b46886f6
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-11 20:07:20 +0600

    about to complete

--- lib/ui/login_signup/widgets/login.dart
+++ lib/ui/login_signup/widgets/login.dart
@@ -11,16 +11,23 @@ class Login extends StatelessWidget{
     return Scaffold(
       backgroundColor: Color(0xff00D09E),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        
 
         children: [
-          Text('Welcome',
-            style: TextStyle(
-              fontSize: 30,
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Welcome',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ),
           ),
-          Flexible(
+          Expanded(
+            flex: 3,
             child: Container(
               decoration: BoxDecoration(
                 color: Color(0xffffffff),
@@ -30,65 +37,67 @@ class Login extends StatelessWidget{
                 ),
               ),
               height: double.infinity,
-              margin: EdgeInsets.only(top: 150),
+              // margin: EdgeInsets.only(top: 150),
               
-              child: Padding(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 50, vertical: 80),
-                child: Form(
-                  child: Column(
-                    children: [
-                      ExtendedTextFormField(
-                        label: 'Username or email',
-                        hint: 'example@example.com',
-                      ),
-                      SizedBox(height: 40,),
-                      ExtendedTextFormField(
-                        label: 'Password', 
-                        hint: 'Enter password',
-                        obscure: true,
-                      ),
-                      SizedBox(height: 50,),
-                      PrimaryButton(
-                        label: 'Log In',
-                        onpressed: (){
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsetsGeometry.symmetric(horizontal: 50, vertical: 80),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        ExtendedTextFormField(
+                          label: 'Username or email',
+                          hint: 'example@example.com',
+                        ),
+                        SizedBox(height: 40,),
+                        ExtendedTextFormField(
+                          label: 'Password', 
+                          hint: 'Enter password',
+                          obscure: true,
+                        ),
+                        SizedBox(height: 50,),
+                        PrimaryButton(
+                          label: 'Log In',
+                          onpressed: (){
+                            
+                          },
+                        ),
+                        SizedBox(height: 19,),
+                        Text('Forgot Passowrd'),
+                        SizedBox(height: 14,),
+                        SecondaryButton(
+                          label: 'Sign Up', 
+                          onpressed: (){
+                            NavigationService().navigateToRoute('signup/');
+                          },
+                        ),
+                        SizedBox(height: 23,),
                           
-                        },
-                      ),
-                      SizedBox(height: 19,),
-                      Text('Forgot Passowrd'),
-                      SizedBox(height: 14,),
-                      SecondaryButton(
-                        label: 'Sign Up', 
-                        onpressed: (){
-                          NavigationService().navigateToRoute('signup/');
-                        },
-                      ),
-                      SizedBox(height: 23,),
-                        
-                      Text('Use Fingerprint To Access'),
-                      SizedBox(height: 28,),    
-                      Text('or sign up with'),
-                      SizedBox(height: 19,),    
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.facebook,
-                          ),
-                          Icon(
-                            Icons.face,
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 20,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Don\'t have an account?'),
-                          Text('SignUp'),
-                        ],
-                      )
-                    ],
+                        Text('Use Fingerprint To Access'),
+                        SizedBox(height: 28,),    
+                        Text('or sign up with'),
+                        SizedBox(height: 19,),    
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.facebook,
+                            ),
+                            Icon(
+                              Icons.face,
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Don\'t have an account?'),
+                            Text('SignUp'),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
--- lib/ui/login_signup/widgets/signup.dart
+++ lib/ui/login_signup/widgets/signup.dart
@@ -9,48 +9,73 @@ class Signup extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      body: Padding(
-        padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Create Account'),
-            Form(
-              child: Column(
-        
-                children: [
-                  ExtendedTextFormField(label: 'Full name', hint: 'example@example.com'),
-                  ExtendedTextFormField(label: 'Email', hint: 'example@example.com'),
-                  ExtendedTextFormField(label: 'Mobile number', hint: '+880 1*******'),
-                  ExtendedTextFormField(label: 'Date of birth', hint: 'DD/MM/YYYY'),
-                  ExtendedTextFormField(label: 'Password', hint: '******', obscure: true,),
-                  ExtendedTextFormField(label: 'Confirm Password', hint: '*******', obscure: true,),
-        
-                  Text('By continuing, you agree to'),
-                  Text('Terms of Use and Privacy Policy'),
-        
-                  PrimaryButton(
-                    label: 'Sign Up',
-                    onpressed: (){
-                      
-                    },
-                  ),
-        
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold( 
+      backgroundColor: Color(0xff00D09E),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                'Create Account',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 30,
+                ),
+              )
+            )
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xffffffff),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            height: double.infinity,
+            
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text('Already have an account?'),
-                      Text('Login'),
+                      ExtendedTextFormField(label: 'Full name', hint: 'example@example.com'),
+                      ExtendedTextFormField(label: 'Email', hint: 'example@example.com'),
+                      ExtendedTextFormField(label: 'Mobile number', hint: '+880 1*******'),
+                      ExtendedTextFormField(label: 'Date of birth', hint: 'DD/MM/YYYY'),
+                      ExtendedTextFormField(label: 'Password', hint: '******', obscure: true,),
+                      ExtendedTextFormField(label: 'Confirm Password', hint: '*******', obscure: true,),
+                        
+                      Text('By continuing, you agree to'),
+                      Text('Terms of Use and Privacy Policy'),
+                        
+                      PrimaryButton(
+                        label: 'Sign Up',
+                        onpressed: (){
+                          
+                        },
+                      ),
+                        
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Already have an account?'),
+                          Text('Login'),
+                        ],
+                      )
+                        
                     ],
                   )
-        
-                ],
-              )
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
